eef667a7da7ed1ac86ad61ae3de03a6ece42b0bd
AbelR <abel.rc2004@gmail.com>
2025-11-04 22:49:24 +0100

    Tercer commit: Perfeccionamos el LWC (lightning web component) para que, al presionar sobre el nombre de una incidencia, se nos abra su página de detalle

--- force-app/main/default/lwc/incidenciasList/incidenciasList.js
+++ force-app/main/default/lwc/incidenciasList/incidenciasList.js
@@ -2,7 +2,15 @@ import { LightningElement, wire } from 'lwc';
 import getIncidencias from '@salesforce/apex/IncidenciaController.getIncidencias';
 
 const COLUMNS = [
-    { label: 'Nombre', fieldName: 'Name' },
+    {
+        label: 'Nombre',
+        fieldName: 'recordLink',
+        type: 'url',
+        typeAttributes: {
+            label: { fieldName: 'Name' },
+            target: '_blank'
+        }
+    },
     { label: 'Estado', fieldName: 'Estado__c' },
     { label: 'Prioridad', fieldName: 'Prioridad__c' },
     { label: 'Fecha de Creación', fieldName: 'Fecha_Creacion__c', type: 'date' }
@@ -10,5 +18,20 @@ const COLUMNS = [
 
 export default class IncidenciasList extends LightningElement {
     columns = COLUMNS;
-    @wire(getIncidencias) incidencias;
+    incidencias;
+    error;
+
+    @wire(getIncidencias)
+    wiredIncidencias({ data, error }) {
+        if (data) {
+            this.incidencias = data.map(inc => ({
+                ...inc,
+                recordLink: '/' + inc.Id
+            }));
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.incidencias = undefined;
+        }
+    }
 }
